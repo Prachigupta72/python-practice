6078f46ded0dce2836ca81ebb768e2bb6e76b322
prachigupta2026 <prachi.gupta2026a@vitstudent.ac.in>
2026-08-09 22:28:23 +0530

    03_simple_calculator.py

---
+++ 03_simple_calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter number 1 : " ))
+b =  int(input("Enter number 2:" ))
+
+total = (a+b)
+product= (a*b)
+diff = (a-b)
+divide = a/b
+print("\nAddition:",total)
+print("Multiplication:",product)
+print("Subtraction:",diff)
+print("Division:",divide)
